e066fd788455bcacd5aee316cc288397c86b62e1
Davi27 <67101009+Davi27@users.noreply.github.com>
2022-04-04 18:53:00 +0100

    Update get_next_line.h
    working

--- get_next_line.h
+++ get_next_line.h
@@ -20,3 +20,26 @@ int		get_size(char *buff);
 int		get_size_linha(char *buff);
 
 #endif
+
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 3
+#endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+char 	*get_next_line(int fd);
+char	*form_line(int fd, char *staticchar);
+char	*complete_line(char *staticchar);
+char	*handle_leftovers(char *staticchar);
+size_t	ft_strlen(char *string);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
