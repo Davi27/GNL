082fc704ca5a137285dd4e17b018f7a71e2ede05
Davi27 <67101009+Davi27@users.noreply.github.com>
2022-04-04 18:52:31 +0100

    Update get_next_line_utils.c
    working version to compare

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -43,3 +43,64 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ptr[i] = '\0';
 	return (ptr);
 }
+
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *string)
+{
+	size_t	i;
+
+	i = 0;
+	if (!string)
+		return (0);
+	while (string[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) == (unsigned char)c)
+			return ((char *)str + i);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)str + i);
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	j;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (ptr == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			ptr[i] = s1[i];
+	while (s2[j] != '\0')
+		ptr[i++] = s2[j++];
+	ptr[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (ptr);
+}
